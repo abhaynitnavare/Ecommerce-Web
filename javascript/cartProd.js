7da54f1d347a2219d4d408135cdb8d3611d634b3
abhaynitnavare <abhaynitnavare0@gmail.com>
2023-07-03 12:03:41 +0530

    seventh commit

--- javascript/cartProd.js
+++ javascript/cartProd.js
@@ -1,77 +1,186 @@
 let sneakes_Shoes = [
     {
 
-        "id":1,
-        "name":"POWER",
-        "img":"https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":2500,
-        "sizes":[6,7,8,9,10]
+        "id": 1,
+        "name": "POWER",
+        "img": "https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 2500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+    
+        "Reviews":"Reviews(46)"
     },
     {
-        "id":2,
-        "name":"JUMP",
-        "img":"https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":1500,
-        "sizes":[6,7,8,9,10]
+        "id": 2,
+        "name": "JUMP",
+        "img": "https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 1500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
-        "id":3,
-        "name":"NIKE",
-        "img":"https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":7500,
-        "sizes":[6,7,8,9,10]
-    },
-    {
-
-        "id":4,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
-    },
-    {
-
-        "id":5,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 3,
+        "name": "NIKE",
+        "img": "https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 7500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":6,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 4,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":7,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 5,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/14188882/pexels-photo-14188882.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":8,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 6,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/6153367/pexels-photo-6153367.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 7,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1478441/pexels-photo-1478441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 8,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/4252950/pexels-photo-4252950.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 9,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/11324524/pexels-photo-11324524.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 10,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/10924183/pexels-photo-10924183.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 11,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/4379226/pexels-photo-4379226.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 12,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1750045/pexels-photo-1750045.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 13,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1266445/pexels-photo-1266445.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 14,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/7524994/pexels-photo-7524994.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load0",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 14,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1580267/pexels-photo-1580267.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+
+    },
+    {
+
+        "id": 14,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/12837963/pexels-photo-12837963.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
     }
 ]
 
-
-
-function getProdList(){
-    let productList = document.querySelector('#productList')
+function newCart(){
     cartIdList = localStorage.getItem('cart-list').split(",")
 
-    console.log(cartIdList)
+    console.log("Cart",cartIdList)
 
     newProdCartList = []
+    // console.log("hiiiiii",newProdCartList)
 
     cartIdList.forEach((id)=>{
     console.log(id)
@@ -82,21 +191,28 @@ function getProdList(){
         else{
             continue
         }
-
     }
  })
-
  console.log(newProdCartList)
+
+ return newProdCartList
+}
+
+function getProdList(){
+    let productList = document.querySelector('#productList')
+
+    let newProdCartList = newCart()
  list =""
  for(let cartData of newProdCartList){
     list += `
-        <li>
-            <img src=${cartData.img} >
-            <h2>${cartData.name}</h2>
-            <h4> Price:${cartData.price}</h4>
-            <button>-<button>
-            <button>+<button>
-        </li>
+        <tr>
+            <td><img src=${cartData.img} ></td>
+            <td> <h2>${cartData.name}</h2> </td>
+            <td> <h4> Price:${cartData.price}</h4> </td>
+            <td> <button> - </button> </td>
+            <td> <button> + </button> </td>
+            <td> <button onclick='deleteProd(${cartData.id})'> <i class="fa-sharp fa-solid fa-trash"></i></button> </td>
+        </tr>
     `
  }
 
@@ -104,6 +220,27 @@ function getProdList(){
 
 
 }
-
-
 getProdList()
+
+
+function deleteProd(id){
+    let newProdList = newCart()
+    let delProList = []
+    console.log("Hello",id,newProdList, newProdList)
+    for(let i in newProdList){
+        if(newProdList[i].id == id){
+            console.log(i)
+            let newData = newProdList.splice(i,1)
+            console.log(newProdList)
+        }
+    }
+
+    for(let data of newProdList){
+        delProList.push(data.id)
+    }
+    localStorage.setItem('cart-list',delProList )
+
+    location.reload()
+
+
+}
--- javascript/main.js
+++ javascript/main.js
@@ -162,7 +162,7 @@ function menu() {
 function Category() {
     let mandrp = document.getElementById('Main-ol')
     // document.getElementById('manudrp').innerHTML
-    let manudrp=document.getElementById('manudrp')
+    let manudrp = document.getElementById('manudrp')
 
     console.log('clucked')
 
@@ -173,101 +173,217 @@ function Category() {
 
     }
     else {
-        document.querySelector('#Main-ol').style.display="none"
+        document.querySelector('#Main-ol').style.display = "none"
 
     }
     // manudrp.innerHTML=mandrp.textContent
 }
 
-function login(){
-    document.getElementById('Localform').style.display='block'
+function login() {
+    document.getElementById('Localform').style.display = 'block'
     console.log('hiii stop')
 
 
 }
 var modal = document.getElementById('Localform');
-window.onclick = function(event){
-    if(event.target == modal){
-        modal.style.display='none'
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none'
     }
 }
+
+
+// ADD TO CART METHOD
 
 let sneakes_Shoes = [
     {
 
-        "id":1,
-        "name":"POWER",
-        "img":"https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":2500,
-        "sizes":[6,7,8,9,10]
+        "id": 1,
+        "name": "POWER",
+        "img": "https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 2500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+    
+        "Reviews":"Reviews(46)"
     },
     {
-        "id":2,
-        "name":"JUMP",
-        "img":"https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":1500,
-        "sizes":[6,7,8,9,10]
+        "id": 2,
+        "name": "JUMP",
+        "img": "https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 1500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
-        "id":3,
-        "name":"NIKE",
-        "img":"https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":7500,
-        "sizes":[6,7,8,9,10]
-    },
-    {
-
-        "id":4,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
-    },
-    {
-
-        "id":5,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 3,
+        "name": "NIKE",
+        "img": "https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 7500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":6,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 4,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":7,
-        "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "id": 5,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/14188882/pexels-photo-14188882.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
     },
     {
 
-        "id":8,
+        "id": 6,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/6153367/pexels-photo-6153367.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 7,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1478441/pexels-photo-1478441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 8,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/4252950/pexels-photo-4252950.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 9,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/11324524/pexels-photo-11324524.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 10,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/10924183/pexels-photo-10924183.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 11,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/4379226/pexels-photo-4379226.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 12,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1750045/pexels-photo-1750045.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 13,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1266445/pexels-photo-1266445.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 14,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/7524994/pexels-photo-7524994.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load0",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+        
+    },
+    {
+
+        "id": 15,
+        "name": "SPEED",
+        "img": "https://images.pexels.com/photos/1580267/pexels-photo-1580267.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
+
+    },
+    {
+
+        "id": 16,
         "name":"SPEED",
-        "img":"https://images.pexels.com/photos/847371/pexels-photo-847371.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "price":5500,
-        "sizes":[6,7,8,9,10]
+        "img": "https://images.pexels.com/photos/12837963/pexels-photo-12837963.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "price": 5500,
+        "sizes": [6, 7, 8, 9, 10],
+        "img1": "https://static.vecteezy.com/system/resources/previews/003/355/389/original/five-5-star-rank-sign-illustration-free-vector.jpg",
+        "Reviews":"Reviews(46)"
     }
+
+
+
 ]
 
 
-let shoesCard  = document.getElementById('shoesCard')
+let shoesCard = document.getElementById('shoesCard')
 let card = ""
-let i=1
-function getDetails(){
-    
-    sneakes_Shoes.forEach((data)=>{
+let i = 1
+function getDetails() {
+    sneakes_Shoes.forEach((data) => {
         console.log(data)
         card += `
             <div class="Scard" id="cards-${i}">
-                    <img src=${data.img} >
+                    <img class="IMg"  src=${data.img} >
                     <h2>${data.name}</h2>
                     <h2>Price: ${data.price}</h2>
                     <h4>Sizes:
@@ -277,9 +393,15 @@ function getDetails(){
                             <span>${data.sizes[3]}</span>
                             <span>${data.sizes[4]}</span>
                     </h4>
-                    <button class="Add_CART" onclick="addtoCart(${data.id})">ADD TO CART</button>   
+                    <button class="Add_CART" onclick="addtoCart(${data.id})">ADD TO CART</button> 
+                    <div class="Revi_div">
+                    <img class="rewiveClass" src=${data.img1}>  
+                    <p>${data.Reviews}
+
+                    </div>
+                    
             </div>
-         ` 
+         `
         i++
         // console.log(card)
     })
@@ -291,32 +413,83 @@ getDetails()
 
 let backet = []
 let localStg = localStorage.getItem('cart-list')
-function addtoCart(val){
-    if(localStg == ""){
+function addtoCart(val) {
+
+    if (localStg == "") {
         backet.push(val)
-        localStorage.setItem('cart-list',backet)
+        localStorage.setItem('cart-list', backet)
     }
-    else{
+
+    else {
         backet = localStorage.getItem('cart-list').split(",")
-        console.log(typeof(backet))
-        backet.push(val)
-        localStorage.setItem('cart-list',backet)
+        console.log(typeof (backet))
+        console.log(val,backet)
+
+        if(backet.includes(String(val))){
+            alert('already exixt')
+        }
+        else{
+            backet.push(val)
+            localStorage.setItem('cart-list', backet)
+        }
     }
+
 }
 
-
 let CartSpan = document.getElementById('Cart-span')
-setInterval(()=>{
+setInterval(() => {
     localStg = localStorage.getItem('cart-list')
-    if(localStg == ""){
-       let  len = localStg.length
+    if (localStg == "") {
+        let len = localStg.length
         CartSpan.innerText = len
     }
-    else{
+    else {
         let len = localStg.split(',').length
         CartSpan.innerText = len
     }
-},500)
+}, 500)
+
+
+
+
+let CartBar = document.querySelector("#cartslide")
+ CartBar.style.display='none'
+ let clbutton = document.getElementsByClassName('Clonfunct')
+ clbutton.style.display='none'
+
+function cartbar(){
+
+    if(CartBar.style.display='none'){
+        CartBar.style.display='block'
+        CartBar.classList.add('tSlide')
+        console.log('Done')
+        
+        if(CartBar.className.includes('tSlide')){
+            console.log('task done')
+        }
+          
+    }
+
+
+
+
+}
+
+
+function closefun(){
+    if (  clbutton.style.display=='none') {
+        console.log ('display none')
+       
+    }
+    else {
+        clbutton.style.display=='none'
+        // CartBar.style.display=='none'
+        console.log('hiiiiiiiiii')
+    }
+    // CartBar.style.display=='none'
+}
+// cartbar()
+closefun()
 
 
 
